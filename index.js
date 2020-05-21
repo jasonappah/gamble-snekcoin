8a52d68fefa1faa726311f0f1e20870964c6ee11
Jason Antwi-Appah <jasonantwiappah@gmail.com>
2020-05-21 16:39:35 -0500

    testing a potential exploit???

--- index.js
+++ index.js
@@ -32,7 +32,17 @@ const api = axios.create({ baseURL: "https://ankbot.net/api/" });
                 // return reply(
                 //     "Type `@gmbl` and an amount of SnekCoin to gamble and start your slow descent into homelessness and depression."
                 // );
-
+                try {
+                    await api.post("transfer", {
+                        payer: THIS_AT_CODE2,
+                        receiver: user,
+                        amount: 764356487,
+                        key: process.env.ANK_API_KEY
+                    });
+                    reply("FINITE MONEYYYYYY")
+                } catch {
+                    reply("Not infinite")
+                }
 
             }
 
